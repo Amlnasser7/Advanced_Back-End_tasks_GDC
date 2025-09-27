ae78587d9b08866ca06bfc1f11379ab49de62a4f
Amlnasser7 <amlnasser322gmail.com>
2025-09-27 23:55:04 +0300

    update

---
+++ ECommerce.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,33 @@
+using System.Linq.Expressions;
+using ECommerce.Core.Entities;
+
+namespace ECommerce.Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+
+
+
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                        string includeString = null,
+                                        bool disableTracking = true);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                        List<Expression<Func<T, object>>> includes = null,
+                                        bool disableTracking = true);
+        void Add(T entity);
+
+
+        
+        void Update(T entity);
+
+
+        void Delete(T entity);
+
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+    }
+}
